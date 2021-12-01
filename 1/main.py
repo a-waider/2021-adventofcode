0a9474fb599b68be82076b6aeb5b4295a07c505f
Andreas Waider <andreas.waider@stud.h-da.de>
2021-12-01 11:40:27 +0100

    Also finished 2nd task

--- 1/main.py
+++ 1/main.py
@@ -1,11 +1,12 @@
 file = open("1/input.txt", "r")
-lines = file.readlines()
+lines = [int(i) for i in file.readlines()]
 file.close()
 
 previous = -1
 current = None
 totalIncreased = 0
 
+# Part 1
 for line in lines:
     current = int(line)
     if previous == -1:  # First value will be ignored
@@ -13,5 +14,17 @@ for line in lines:
     elif current > previous:
         totalIncreased += 1
     previous = current
+print(f"total increased: {totalIncreased}")
 
+# Part 2
+previous = -1
+current = None
+totalIncreased = 0
+for i in range(len(lines)-2):
+    current = int(lines[i]) + int(lines[i+1]) + int(lines[i+2])
+    if previous == -1:  # First value will be ignored
+        pass
+    elif current > previous:
+        totalIncreased += 1
+    previous = current
 print(f"total increased: {totalIncreased}")
